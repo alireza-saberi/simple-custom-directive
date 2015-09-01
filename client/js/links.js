49320a87d65b7c96a414cdd853557189bde9847f
alireza-saberi <saberi.ar.eng@gmail.com>
2015-09-01 17:54:11 -0400

    chore, feat: the whole cusotm directive is formed
    Very simple custom directive is formed

---
+++ client/js/links.js
@@ -0,0 +1,21 @@
+(function(){
+	var linkDemo = function(){
+		return {
+				restrict: 'A',
+				link: function(scope, elem, attrs){
+					elem.on('click', function(){
+						elem.html('You clicked me!');
+					});
+					elem.on('mouseenter', function(){
+						elem.css('background-color', 'yellow');
+					});
+					elem.on('mouseleave', function(){
+						elem.css('background-color', 'white');
+					});
+				}
+
+		};
+	};
+	angular.module('module').directive('linkDemo', linkDemo);
+
+}());
